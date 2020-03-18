202fb8f0fe1e8c9bd94d24cc339c8f396afac58b
unknown <areyes986@gmail.com>
2020-03-17 22:45:35 -0700

    started challenge

---
+++ code-challenges/ArrayShift/ArrayShift/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ArrayShift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] oldArray = new int[] { 1, 2, 4, 5 };
+            InsertShiftArray(oldArray, 3);
+        }
+
+        static int[] InsertShiftArray(int[]array, int value)
+        {
+            int[] newArray = new int[array.Length + 1];
+            decimal middleArray = Math.Ceiling(newArray.Length / 2m);
+            int val = 0;
+
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                if (middleArray == i )
+                {
+                    newArray[i] = value;
+                    val--;
+                }
+                else
+                {
+                    val++;
+                }
+            }
+            return newArray;
+        }
+    }
+}
