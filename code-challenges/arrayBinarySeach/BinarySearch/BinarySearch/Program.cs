5b3547311e6f0609545ad373a2618a04879f5566
Allyson Reyes <areyes986@gmail.com>
2020-03-18 21:39:20 -0700

    started the readme and structure of code in program.cs

---
+++ code-challenges/arrayBinarySeach/BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 3, 6, 7, 5, 2 };
+            int key = 5;
+            BinarySearch(array, key);
+        }
+
+        static void BinarySearch(int[] myArray, int key)
+        {
+            for (int i = 0; i < myArray.Length; i++)
+            {
+
+                if (myArray[i] == key)
+                {
+                    Console.WriteLine(i);
+                }
+                else if (myArray[i] != key)
+                {
+
+                }
+            }
+
+        }
+    }
+}
