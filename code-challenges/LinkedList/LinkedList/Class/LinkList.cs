4d780cfd818082500b74b7fe5e93284a04adc76f
Allyson Reyes <areyes986@gmail.com>
2020-03-23 13:39:02 -0700

    finished linked-list

---
+++ code-challenges/LinkedList/LinkedList/Class/LinkList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList.Class
+{
+    public class LinkList
+    {
+        public Node Head { get; set; }
+        public Node Current { get; set; }
+
+
+        /// <summary>
+        /// We are inserting the nodes inputted value as the head
+        /// </summary>
+        /// <param name="value"> The input </param>
+        public void Insert(int value)
+        {
+            Node node = new Node();
+            //the inputted data is set to value
+            node.Value = value;
+            // this sets next to head
+            node.Next = Head;
+            // set the new node as head
+            Head = node;
+        }
+
+        /// <summary>
+        /// This tranveres through the linklist to look for the value.
+        /// if the current value equals the inserted value, it returns true, if not false.
+        /// </summary>
+        /// <param name="value"> the searched value </param>
+        /// <returns></returns>
+        public bool Includes(int value)
+        {
+            Current = Head;
+            while (Current != null)
+            {
+                if( Current.Value == value)
+                {
+                    return true;
+                }
+            Current = Current.Next;
+            }
+            return false;
+        }
+
+
+        /// <summary>
+        /// This prints out the nodes that are in the linkedlink
+        /// </summary>
+        /// <returns> returns the appended string</returns>
+        public override string ToString()
+        {
+            Current = Head;
+            StringBuilder sb = new StringBuilder();
+            while (Current != null)
+            {
+                sb.Append($"{Current.Value} -> ");
+                Current = Current.Next;
+            }
+
+            // we are at null at this point
+            sb.Append("NULL");
+            return sb.ToString();
+        }
+
+    }
+
+
+
+
+}
---
+++ code-challenges/LinkedList/linkedlist.md
@@ -0,0 +1,42 @@
+# Linked List
+
+*Author: Allyson Reyes*
+
+---
+
+### Problem Domain
+Create a methods when instantiating the node object, you create the head node. Created 3 methods to insert, includes, and to string.
+The insert should add nodes into the linked list, includes searches through and see if the value exists, and to string prints out the nodes in the linkedlist until null.
+
+
+---
+
+### Inputs and Expected Outputs
+
+| Input | Expected Output |
+| :----------- | :----------- |
+| Node | Head-Node |
+
+
+
+---
+
+
+### Result
+![Results](../../assets/resultsLinkedLists.png)
+
+### Resources
+Referenced from the demo code provided from Amanda.  
+[Link to Demo Code](https://github.com/codefellows/seattle-dotnet-401d10/tree/master/Class05/demo/LinkedList)
+
+
+
+
+---
+
+### Change Log  
+1.5: *finished readme* - 3/23/2020  
+1.4  *finished toString method* - 3/23/2020  
+1.3: *finished the includes method* - 3/23/2020  
+1.2: *finished the insert method* - 3/23/2020  
+1.1: *started off with the tests* - 3/23/2020  
